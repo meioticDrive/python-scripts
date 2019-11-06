c949a34f7d149896f61d12cbbde49b825735ea19
Michael Zwick <mzwick@dhcp-231-120.whitehead.emory.edu>
2019-11-06 09:23:33 -0500

    Script to parse Bystro output

---
+++ parseBystro.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# The purpose of this script is to parse the most relevant columns from Bystro output
+
+
+def main():
+	print("Hello World!")
+	print("Hello World!")
+	print("Hello World!")
+main()
