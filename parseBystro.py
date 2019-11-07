3b76cc3e65cb2e82c91d0cdb7885444e7805f4fe
meioticDrive <mzwick@emory.edu>
2019-11-07 16:41:38 -0500

    Latest Version of parseBystro

--- parseBystro.py
+++ parseBystro.py
@@ -13,7 +13,7 @@ def main():
     outfile = open(outfilename,"w")
     
     #Print column titles
-    print("Chromosome\t", "Position\t", "Type\t", "phastCons\t", "phyloP\t", "cadd\t", "dbSNP.name\t", "dbSNP.func\t", "gnomad.genomes.id\t", "gnomad.genomes.af\t", "refSeq.siteType\t", "refSeq.exonicAllelefunction\t", "refSeq.description\t", file=outfile)
+    print("Chromosome\t", "Position\t", "Type\t", "phastCons\t", "phyloP\t", "cadd\t", "dbSNP.name\t", "dbSNP.func\t", "gnomad.genomes.id\t", "gnomad.genomes.af\t", "refSeq.siteType\t", "refSeq.exonicAllelefunction\t", "refSeq.name\t", "refSeq.name2\t", "refSeq.description\t", file=outfile)
     #Labels "refSeq.siteType", "refSeq.exonicAlleleFunction", "refSeq.description", "refSeq.name", "refSeq.name2", "phyloP", "CADD", "dbSNP.name", "dbSNP.func", "gnomad.genomes.id", "gnomad.genomes.af", "gnomad.exomes.id", "gnomad.exomes.af", "refSeq.clinvar.alleleID", "refSeq.clinvar.phenotypeList", "refSeq.clinvar.clinicalSignificance"
     
     # Process the input file
@@ -34,7 +34,7 @@ def main():
         #Print desired columns from bystro
         #print(line[:-1])
         temp = line.split('\t')
-        print(temp[0], "\t", temp[1], "\t", temp[2], "\t", temp[45], "\t", temp[46], "\t", temp[47], "\t", temp[48], "\t", temp[52], "\t", temp[66], "\t", temp[67], "\t", temp[16], "\t", temp[17], "\t", temp[30], "\t", file=outfile)
+        print(temp[0], "\t", temp[1], "\t", temp[2], "\t", temp[45], "\t", temp[46], "\t", temp[47], "\t", temp[48], "\t", temp[52], "\t", temp[66], "\t", temp[67], "\t", temp[16], "\t", temp[17], "\t", temp[32], "\t", temp[33], "\t", temp[30], "\t", file=outfile)
         # , temp[32], temp[33], temp[36], temp[37], temp[38], temp[88], temp[89] 
     # close both files
     infile.close()
